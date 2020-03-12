8cc4324fed146058be7b9aa4b8d0fc45f169c45b
Thai Pangsakulyanont <dtinth@spacet.me>
2020-03-13 02:08:46 +0700

    Update jsonviewer.config.js

--- projects/jsonviewer.config.js
+++ projects/jsonviewer.config.js
@@ -1,6 +1,6 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
-    await page.setViewport({ width: 1280, height: 720 })
+    await page.setViewport({ width: 1280, height: 640 })
     await page.goto('http://json.spacet.me')
     await capture(page, 'initial')
   },
