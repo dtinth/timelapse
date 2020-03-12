71ff0a03ea940c5dbfc367d183136dfe43f4f103
Thai Pangsakulyanont <dtinth@spacet.me>
2020-03-13 00:39:41 +0700

    Show demo mode for voice-recorder

--- projects/voice-recorder.config.js
+++ projects/voice-recorder.config.js
@@ -1,7 +1,7 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 800 })
-    await page.goto('http://vox.spacet.me')
+    await page.goto('https://vox.spacet.me/?demo')
     await capture(page, 'initial')
   },
 })
