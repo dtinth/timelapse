010812fba4be13089e0dc1af25f7ca15336c532b
Thai Pangsakulyanont <dtinth@spacet.me>
2020-03-12 11:25:59 +0700

    Add suffix to images

--- projects/bpm.config.js
+++ projects/bpm.config.js
@@ -2,6 +2,6 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
     await page.goto('http://bpm.spacet.me')
-    await capture(page)
+    await capture(page, 'initial')
   },
 })
--- projects/voice-recorder.config.js
+++ projects/voice-recorder.config.js
@@ -2,6 +2,6 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto('http://vox.spacet.me')
-    await capture(page)
+    await capture(page, 'initial')
   },
 })
