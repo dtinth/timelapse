f5a82e23b2d0a4fe55edbbd97c51ef896a595456
Thai Pangsakulyanont <dtinth@spacet.me>
2020-03-14 01:57:06 +0700

    Update voice-recorder.config.js

--- projects/voice-recorder.config.js
+++ projects/voice-recorder.config.js
@@ -1,6 +1,6 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
-    await page.setViewport({ width: 1280, height: 800 })
+    await page.setViewport({ width: 1280, height: 720 })
     await page.goto('https://vox.spacet.me/?demo')
     await capture(page, 'initial')
   },
