48c1f1778ad19ece2a7ddd7f8a048d6201ceab43
Thai Pangsakulyanont <dtinth@spacet.me>
2020-04-08 22:03:21 +0700

    Update img.config.js

--- projects/img.config.js
+++ projects/img.config.js
@@ -3,5 +3,8 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
     await page.setViewport({ width: 914, height: 512, deviceScaleFactor: 2 })
     await page.goto('https://img.spacet.me')
     await capture(page, 'initial')
+    await page.setViewport({ width: 914, height: 914, deviceScaleFactor: 2 })
+    await page.goto('https://img.spacet.me/?demo=1')
+    await capture(page, 'demo')
   },
 })
