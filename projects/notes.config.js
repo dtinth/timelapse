5c9efd891a828f1f3c9d82fac79e7554484bcc05
Thai Pangsakulyanont <dtinth@spacet.me>
2020-12-11 21:55:03 +0000

    Add notes project

---
+++ projects/notes.config.js
@@ -0,0 +1,7 @@
+module.exports = /** @type {TimelapseProjectConfig} */ ({
+  async run({ page, capture }) {
+    await page.setViewport({ width: 1280, height: 720 })
+    await page.goto('https://notes.dt.in.th/20201205T185137Z4313')
+    await capture(page, 'about')
+  },
+})
