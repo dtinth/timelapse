00cd8efa7175ccc7eed2e80a08dae5f9d897dfaf
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-04 12:08:57 +0000

    wonderful.software

---
+++ projects/wonderful.software.config.js
@@ -0,0 +1,8 @@
+module.exports = /** @type {TimelapseProjectConfig} */ ({
+  async run({ page, capture }) {
+    await page.setViewport({ width: 1280, height: 720 })
+
+    await page.goto('https://wonderful.software/')
+    await capture(page, 'home')
+  },
+})
