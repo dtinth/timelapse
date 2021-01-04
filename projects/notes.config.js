346680c3b00a4b1a873ea94ef9b7b1cb8a490246
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-04 11:15:05 +0000

    Add notes homepage

--- projects/notes.config.js
+++ projects/notes.config.js
@@ -1,7 +1,11 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
+
     await page.goto('https://notes.dt.in.th/20201205T185137Z4313')
     await capture(page, 'about')
+
+    await page.goto('https://notes.dt.in.th/')
+    await capture(page, 'home')
   },
 })
