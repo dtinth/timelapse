924161bdc7eb26dcfc8e0ca8e1bf09ddb0ba533f
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-18 19:32:09 +0700

    Update webring.wonderful.software.config.js

--- projects/webring.wonderful.software.config.js
+++ projects/webring.wonderful.software.config.js
@@ -2,7 +2,7 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
 
-    await page.goto('https://webring.wonderful.software/#wonderful.software')
+    await page.goto('https://webring.wonderful.software/#/wonderful.software')
     await capture(page, 'index')
   },
 })
