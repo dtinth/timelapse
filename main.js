04a49ad55a8ce708bda419499bd3c2302312e911
Thai Pangsakulyanont <dtinth@spacet.me>
2021-07-15 20:04:53 +0000

    Improve performance and stability
    - Run each stage as separate commands: capture, update, commit
    - Take 2 screenshots and use the majority (incl original)

--- main.js
+++ main.js
@@ -7,37 +7,209 @@ const imagemin = require('imagemin')
 const imageminPngquant = require('imagemin-pngquant')
 const mkdirp = require('mkdirp')
 const execa = require('execa')
+const yargs = require('yargs')
+const pMap = require('p-map')
 
-async function main() {
-  const browser = await puppeteer.launch({
-    args: ['--single-process'],
-  })
-  try {
-    if (process.argv[2]) {
-      await runProject(browser, path.basename(process.argv[2], '.config.js'))
-    } else {
-      for (const configFile of glob.sync('projects/*.config.js')) {
+function main() {
+  yargs
+    .demandCommand()
+    .strict()
+    .help()
+    .command(
+      'capture <project>',
+      'Capture screenshot of a project',
+      {
+        project: {
+          type: 'string',
+          description: 'The project to run',
+        },
+        n: {
+          type: 'number',
+          default: 1,
+          description: 'The output number',
+        },
+      },
+      async (args) => {
+        const browser = await puppeteer.launch({
+          args: ['--single-process'],
+        })
         try {
-          const projectName = path.basename(configFile, '.config.js')
-          console.log(`# ${projectName}`)
-          const changes = await runProject(browser, projectName)
-          if (changes.length > 0 && process.env.CI) {
-            await execa('git', ['add', ...changes], { stdio: 'inherit' })
-            await execa(
-              'git',
-              ['commit', '-m', `📸 ${new Date().toJSON()} [${projectName}]`],
-              { stdio: 'inherit' },
-            )
-          }
+          const projectName = args.project
+          const outputDir = `projects-local/${args.n}`
+          await runProject(browser, projectName, outputDir)
         } catch (error) {
-          console.error(error)
-          process.exitCode = 1
+          console.error(
+            `Unable to capture project ${args.project}[${args.n}] due to error:`,
+            error,
+          )
+        } finally {
+          await browser.close()
         }
-      }
-    }
-  } finally {
-    await browser.close()
+      },
+    )
+    .command(
+      'capture-all',
+      'Capture all screenshots',
+      {
+        concurrency: {
+          type: 'number',
+          default: 4,
+          description: 'How many concurrent captures to use',
+        },
+      },
+      async (args) => {
+        const taskSets = [[], []]
+        for (const projectName of getAllProjectNames()) {
+          taskSets[0].push({ projectName, n: 1 })
+          taskSets[1].push({ projectName, n: 2 })
+        }
+        const tasks = taskSets.flatMap((s) => s)
+        let done = 0
+        await pMap(
+          tasks,
+          async ({ projectName, n }) => {
+            const capture = () =>
+              execa('node', ['main', 'capture', projectName, '-n', n], {
+                stdio: 'inherit',
+                reject: false,
+              })
+            let result = await capture()
+            if (result.exitCode !== 0) {
+              // Retry one more time
+              result = await capture()
+            }
+            done++
+            console.log(`Finished ${projectName}[${n}] ${done}/${tasks.length}`)
+          },
+          { concurrency: args.concurrency },
+        )
+      },
+    )
+    .command(
+      'update',
+      'Updates the screenshot if changed',
+      {
+        dryRun: {
+          type: 'boolean',
+          default: false,
+          alias: 'n',
+        },
+      },
+      async (args) => {
+        const actions = []
+        for (const projectName of getAllProjectNames()) {
+          const imageFilenames = Array.from(
+            new Set(
+              glob
+                .sync(`projects-local/*/${projectName}_*.png`)
+                .map((m) => path.basename(m)),
+            ),
+          )
+          for (const imageFilename of imageFilenames) {
+            const target = `projects/${imageFilename}`
+            const oldImage = new ImageFile(target)
+            const newImage1 = new ImageFile(`projects-local/1/${imageFilename}`)
+            const newImage2 = new ImageFile(`projects-local/2/${imageFilename}`)
+            if (!oldImage.exists()) {
+              if (newImage2.exists()) {
+                console.log(`* Create "${target}" from "${newImage2.path}"`)
+                actions.push(() => newImage2.saveTo(target))
+              } else if (newImage1.exists()) {
+                console.log(`* Create "${target}" from "${newImage1.path}"`)
+                actions.push(() => newImage1.saveTo(target))
+              } else {
+                console.log(`* Cannot find images for ${target}`)
+              }
+            } else {
+              if (
+                (await newImage1.isSimilarTo(oldImage)) ||
+                (await newImage2.isSimilarTo(oldImage))
+              ) {
+                console.log(`* Up-to-date: "${target}"`)
+              } else if (newImage2.exists()) {
+                console.log(`* Update "${target}" from "${newImage2.path}"`)
+                actions.push(() => newImage2.saveTo(target))
+              } else if (newImage1.exists()) {
+                console.log(`* Update "${target}" from "${newImage1.path}"`)
+                actions.push(() => newImage1.saveTo(target))
+              } else {
+                console.log(`* Cannot find new image for ${target}`)
+              }
+            }
+          }
+        }
+        if (!args.dryRun) {
+          for (const action of actions) {
+            await action()
+          }
+        }
+      },
+    )
+    .command(
+      'commit',
+      'Commits the updated screenshots',
+      {
+        dryRun: {
+          type: 'boolean',
+          default: false,
+          alias: 'n',
+        },
+      },
+      async (args) => {
+        const changedFiles = (
+          await execa('git', ['status', '--porcelain'])
+        ).stdout
+          .split('\n')
+          .map((line) => line.trim().replace(/^\S+\s+/, ''))
+        const command = async (exe, ...argv) => {
+          console.log(JSON.stringify([exe, ...argv]))
+          if (!args.dryRun) {
+            await execa(exe, argv, { stdio: 'inherit' })
+          }
+        }
+        const prefix = `📸 ${new Date().toJSON()}`
+        for (const projectName of getAllProjectNames()) {
+          const filesToCommit = changedFiles.filter((file) =>
+            file.startsWith(`projects/${projectName}_`),
+          )
+          if (filesToCommit.length === 0) {
+            continue
+          }
+          await command('git', 'add', ...filesToCommit)
+          await command('git', 'commit', '-m', `${prefix} [${projectName}]`)
+        }
+      },
+    )
+    .parse()
+}
+
+class ImageFile {
+  constructor(path) {
+    this.path = path
   }
+  exists() {
+    return fs.existsSync(this.path)
+  }
+  async saveTo(target) {
+    let buffer = fs.readFileSync(this.path)
+    buffer = await optimize(buffer)
+    fs.writeFileSync(target, buffer)
+    console.log(`* Saved "${target}"`)
+  }
+  async isSimilarTo(anotherImage) {
+    if (!this.exists() || !anotherImage.exists()) {
+      return false
+    }
+    let a = await jimp.read(this.path)
+    let b = await jimp.read(anotherImage.path)
+    return !areImagesDifferent(a, b)
+  }
+}
+
+function getAllProjectNames() {
+  return glob
+    .sync('projects/*.config.js')
+    .map((configFile) => path.basename(configFile, '.config.js'))
 }
 
 function areImagesDifferent(a, b) {
@@ -65,8 +237,9 @@ async function optimize(buffer) {
 /**
  * @param {import('puppeteer').Browser} browser
  * @param {string} projectName
+ * @param {string} outputDir
  */
-async function runProject(browser, projectName) {
+async function runProject(browser, projectName, outputDir) {
   const configFile = `projects/${projectName}.config.js`
   /** @type {TimelapseProjectConfig} */
   const config = require(fs.realpathSync(configFile))
@@ -80,9 +253,8 @@ async function runProject(browser, projectName) {
       page,
       capture: async (page, screenshotName = '') => {
         const suffix = `${screenshotName ? '_' : ''}${screenshotName}`
-        const dir = process.env.GITHUB_ACTIONS ? 'projects' : 'projects-local'
-        mkdirp(dir)
-        const target = `${dir}/${projectName}${suffix}.png`
+        mkdirp.sync(outputDir)
+        const target = `${outputDir}/${projectName}${suffix}.png`
         const getScreenshot = () =>
           page.screenshot({ encoding: 'binary', type: 'png' })
 
@@ -104,23 +276,9 @@ async function runProject(browser, projectName) {
           }
         }
 
-        screenshot = await optimize(screenshot)
-
-        if (!fs.existsSync(target)) {
-          console.log('Create: "%s"', target)
-          fs.writeFileSync(target, screenshot)
-          changedFiles.push(target)
-        } else {
-          const existingImage = await jimp.read(target)
-          const latestImage = await jimp.read(screenshot)
-          if (areImagesDifferent(existingImage, latestImage)) {
-            fs.writeFileSync(target, screenshot)
-            console.log('Update: "%s"', target)
-            changedFiles.push(target)
-          } else {
-            console.log('Up-to-date: "%s"', target)
-          }
-        }
+        console.log('Save screenshot to "%s"', target)
+        fs.writeFileSync(target, screenshot)
+        changedFiles.push(target)
       },
       css: async (stylesheet) => {
         await page.evaluate(async (stylesheet) => {
