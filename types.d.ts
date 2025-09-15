bb9d51fe5769946ca3a6d1b8a0914aa89595e6dd
Thai Pangsakulyanont <dtinth@spacet.me>
2025-09-15 07:46:23 +0000

    upgrade Puppeteer and Node

--- types.d.ts
+++ types.d.ts
@@ -3,7 +3,6 @@ type Page = import('puppeteer').Page
 
 interface TimelapseProjectConfig {
   run: (context: {
-    browser: Browser
     page: Page
     capture: (page: Page, name?: string) => Promise<void>
     css: (styles: string) => Promise<void>
